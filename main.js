87218d38171b3a1eb58ef1b956a4c3c2e3e3833e
jonathan <97814319+jonathansfx@users.noreply.github.com>
2022-12-29 12:42:25 -0700

    asdfasfd

---
+++ main.js
@@ -0,0 +1,15 @@
+// Btn Event Listenerr
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+// HTML Var
+let a = document.getElementById("aEl").value;
+let b = document.getElementById("bEl").value;
+let h = document.getElementById("hEl").value;
+
+// Process
+let area = (("a" + "b")/2) * "h";
+
+// Output
+document.getElementById("output").innerHTML = area;
+}
